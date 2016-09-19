ac0ee6b9940df47c6cd6a7b3a685187b400f25d3
Ryan Gorley <ryan@dijt.co>
2016-09-18 19:41:56 -0600

    Just some random changes to test

---
+++ js/UserLib.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    //Wait for Pinegrow to wake-up
+    $("body").one("pinegrow-ready", function(e, pinegrow) {
+
+        //Create new Pinegrow framework object
+        var f = new PgFramework("UserLib", "UserLib");
+
+        //This will prevent activating multiple versions of this framework being loaded
+        f.type = "UserLib";
+        f.allow_single_type = true;
+        f.user_lib = true
+
+        //Tell Pinegrow about the framework
+        pinegrow.addFramework(f);
+            
+        var section = new PgFrameworkLibSection("UserLib_lib", "Components");
+        //Pass components in array
+        section.setComponentTypes([]);
+
+        f.addLibSection(section);
+   });
+});
